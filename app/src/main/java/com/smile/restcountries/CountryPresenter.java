c4b8df2f9b9b6e0169412d6ace2b7c37d4f656fb
smilecs <mumene@gmail.com>
2018-08-06 22:38:38 +0100

    initial commit

---
+++ app/src/main/java/com/smile/restcountries/CountryPresenter.java
@@ -0,0 +1,81 @@
+package com.smile.restcountries;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import java.io.InputStreamReader;
+import java.net.URL;
+
+
+public class CountryPresenter {
+    public void getCountryDataList() {
+
+    }
+
+    private void doRequest(URL url) throws IOException {
+        InputStream stream = null;
+        HttpsURLConnection connection = null;
+        String result = null;
+        try {
+            connection = (HttpsURLConnection) url.openConnection();
+            // Timeout for reading InputStream arbitrarily set to 3000ms.
+            connection.setReadTimeout(3000);
+            // Timeout for connection.connect() arbitrarily set to 3000ms.
+            connection.setConnectTimeout(3000);
+            // For this use case, set HTTP method to GET.
+            connection.setRequestMethod("GET");
+            // Already true by default but setting just in case; needs to be true since this request
+            // is carrying an input (response) body.
+            connection.setDoInput(true);
+            // Open communications link (network traffic occurs here).
+            connection.connect();
+            ///publishProgress(DownloadCallback.Progress.CONNECT_SUCCESS);
+            int responseCode = connection.getResponseCode();
+            if (responseCode != HttpsURLConnection.HTTP_OK) {
+                throw new IOException("HTTP error code: " + responseCode);
+            }
+            // Retrieve the response body as an InputStream.
+            stream = connection.getInputStream();
+            if (stream != null) {
+                // Converts Stream to String with max length of 500.
+                //result = readStream(stream, 500);
+            }
+        } finally {
+            // Close Stream and disconnect HTTPS connection.
+            if (stream != null) {
+                stream.close();
+            }
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+        //   return result;
+    }
+
+    private String readStream(InputStream stream, int maxLength) throws IOException {
+        String result = null;
+        // Read InputStream using the UTF-8 charset.
+        InputStreamReader reader = new InputStreamReader(stream, "UTF-8");
+        // Create temporary buffer to hold Stream data with specified max length.
+        char[] buffer = new char[maxLength];
+        // Populate temporary buffer with Stream data.
+        int numChars = 0;
+        int readSize = 0;
+        while (numChars < maxLength && readSize != -1) {
+            numChars += readSize;
+            int pct = (100 * numChars) / maxLength;
+            readSize = reader.read(buffer, numChars, buffer.length - numChars);
+        }
+        if (numChars != -1) {
+            // The stream was not empty.
+            // Create String that is actual length of response body if actual length was less than
+            // max length.
+            numChars = Math.min(numChars, maxLength);
+            result = new String(buffer, 0, numChars);
+        }
+        return result;
+    }
+}
+
---
+++ app/src/main/java/com/smile/restcountries/CountryViewModel.java
@@ -0,0 +1,9 @@
+package com.smile.restcountries;
+
+import android.arch.lifecycle.ViewModel;
+
+public class CountryViewModel extends ViewModel{
+
+
+
+}
